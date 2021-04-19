8b3ad47357eba5b158d25cf04523a7e37c9cfc4e
David Cermak <cermak@espressif.com>
2021-04-19 11:40:50 +0200

    Add license file and fix license headers of tests and mocks

--- esp_modem/examples/ap_to_pppos/main/NetworkDCE.cpp
+++ esp_modem/examples/ap_to_pppos/main/NetworkDCE.cpp
@@ -1,6 +1,12 @@
-//
-// Created by david on 3/25/21.
-//
+/*  softAP to PPPoS Example (modem_board)
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
 #include "cxx_include/esp_modem_dte.hpp"
 #include "esp_modem_config.h"
 #include "cxx_include/esp_modem_api.hpp"
--- esp_modem/test/target/main/pppd_test.cpp
+++ esp_modem/test/target/main/pppd_test.cpp
@@ -1,4 +1,13 @@
-#include <string.h>
+/*  pppd test
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <cstring>
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_log.h"
