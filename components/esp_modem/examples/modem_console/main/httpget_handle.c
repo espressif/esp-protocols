e026a82d789c65c76ead2fefb6b4a37bd1be2c82
Vladimir Chistyakov <b1ackviking@outlook.com>
2022-01-28 19:48:08 +0700

    fix: missing default statement in a switch in the modem_console example

--- components/esp_modem/examples/modem_console/main/httpget_handle.c
+++ components/esp_modem/examples/modem_console/main/httpget_handle.c
@@ -46,6 +46,7 @@ static esp_err_t http_event_handler(esp_http_client_event_t *evt)
         case HTTP_EVENT_DISCONNECTED:
             ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
             break;
+        default: break;
     }
     return ESP_OK;
 }
