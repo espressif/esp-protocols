ApexAI/asio

Add C++11 version of SOCKS4 example.

asio/boostify.pl
@@ -530,6 +530,7 @@ sub copy_examples
       "src/examples/cpp11/local",
       "src/examples/cpp11/multicast",
       "src/examples/cpp11/nonblocking",
+      "src/examples/cpp11/socks",
       "src/examples/cpp11/spawn",
       "src/examples/cpp11/ssl",
       "src/examples/cpp11/timeouts",

espressif/esp-protocols

asio/boostify.pl
@@ -530,6 +530,8 @@ sub copy_examples
       "src/examples/cpp11/local",
       "src/examples/cpp11/multicast",
       "src/examples/cpp11/nonblocking",
+      "src/examples/cpp11/operations",
+      "src/examples/cpp11/socks4",
       "src/examples/cpp11/spawn",
       "src/examples/cpp11/ssl",
       "src/examples/cpp11/timeouts",