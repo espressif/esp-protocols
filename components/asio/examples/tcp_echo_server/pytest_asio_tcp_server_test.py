682e73e3ab4afd447a0158d812483a2e95d40efe
Suren Gabrielyan <suren.gabrielyan@espressif.com>
2022-09-13 19:14:10 +0400

    ASIO: Example tests integration

--- components/asio/examples/tcp_echo_server/pytest_asio_tcp_server_test.py
+++ components/asio/examples/tcp_echo_server/pytest_asio_tcp_server_test.py
@@ -1,12 +1,10 @@
 import os
 import re
 import socket
+from pytest_embedded import Dut
 
-import ttfw_idf
 
-
-@ttfw_idf.idf_example_test(env_tag='Example_WIFI_Protocols')
-def test_examples_protocol_asio_tcp_server(env, extra_data):
+def test_examples_protocol_asio_tcp_server(dut):
     """
     steps: |
       1. join AP
@@ -16,20 +14,13 @@ def test_examples_protocol_asio_tcp_server(env, extra_data):
       5. Test evaluates received test message on server stdout
     """
     test_msg = b'echo message from client to server'
-    dut1 = env.get_dut('tcp_echo_server', 'examples/protocols/asio/tcp_echo_server', dut_class=ttfw_idf.ESP32DUT)
-    # check and log bin size
-    binary_file = os.path.join(dut1.app.binary_path, 'asio_tcp_echo_server.bin')
-    bin_size = os.path.getsize(binary_file)
-    ttfw_idf.log_performance('asio_tcp_echo_server_bin_size', '{}KB'.format(bin_size // 1024))
-    # 1. start test
-    dut1.start_app()
     # 2. get the server IP address
-    data = dut1.expect(re.compile(r' IPv4 address: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)'), timeout=30)
+    data = dut.expect(re.compile(b' IPv4 address: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)'), timeout=30).group(1).decode()
     # 3. create tcp client and connect to server
-    dut1.expect('ASIO engine is up and running', timeout=1)
+    dut.expect('ASIO engine is up and running', timeout=1)
     cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cli.settimeout(30)
-    cli.connect((data[0], 2222))
+    cli.connect((data, 2222))
     cli.send(test_msg)
     data = cli.recv(1024)
     # 4. check the message received back from the server
@@ -40,8 +31,5 @@ def test_examples_protocol_asio_tcp_server(env, extra_data):
         print('Failure!')
         raise ValueError('Wrong data received from asi tcp server: {} (expected:{})'.format(data, test_msg))
     # 5. check the client message appears also on server terminal
-    dut1.expect(test_msg.decode())
+    dut.expect(test_msg.decode())
 
-
-if __name__ == '__main__':
-    test_examples_protocol_asio_tcp_server()
---
+++ components/asio/examples/udp_echo_server/pytest_asio_udp_server_test.py
@@ -0,0 +1,33 @@
+import os
+import re
+import socket
+
+import pytest
+from pytest_embedded import Dut
+
+def test_examples_protocol_asio_udp_server(dut):
+    """
+    steps: |
+      1. join AP
+      2. Start server
+      3. Test connects to server and sends a test message
+      4. Test evaluates received test message from server
+      5. Test evaluates received test message on server stdout
+    """
+    test_msg = b'echo message from client to server'
+    data = dut.expect(re.compile(b' IPv4 address: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)'), timeout=30).group(1).decode()
+    dut.expect('ASIO engine is up and running', timeout=1)
+    cli = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    cli.settimeout(30)
+    cli.connect((data, 2222))
+    cli.send(test_msg)
+    data = cli.recv(1024)
+    # 4. check the message received back from the server
+    if (data == test_msg):
+        print('PASS: Received correct message')
+        pass
+    else:
+        print('Failure!')
+        raise ValueError('Wrong data received from asio udp server: {} (expected:{})'.format(data, test_msg))
+    # 5. check the client message appears also on server terminal
+    dut.expect(test_msg.decode())
