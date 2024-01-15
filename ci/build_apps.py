47736a2556bb7603c49fd950e64352f177493f97
Abhik Roy <abhik.roy@espressif.com>
2024-01-16 00:37:32 +1100

    fix(common): Fixed scripts for build test failure

--- ci/build_apps.py
+++ ci/build_apps.py
@@ -10,6 +10,8 @@ import sys
 
 from idf_build_apps import build_apps, find_apps, setup_logging
 from idf_build_apps.constants import SUPPORTED_TARGETS
+from packaging import version
+from pkg_resources import get_distribution
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -17,6 +19,12 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument('paths', nargs='+', help='Paths to the apps to build.')
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        help='Increase the LOGGER level of the script. Can be specified multiple times.',
+    )
     parser.add_argument(
         '-t',
         '--target',
@@ -28,6 +36,8 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--delete', action='store_true', help='Delete build artifacts')
     parser.add_argument('-c', '--recursive', action='store_true', help='Build recursively')
     parser.add_argument('-l', '--linux', action='store_true', help='Include linux build (dont check warnings)')
+    parser.add_argument('--preserve-all', action='store_true', help='Preserve the binaries for all apps when specified.')
+    parser.add_argument('--pytest-apps', action='store_true', help='Only build apps required by pytest scripts.')
     args = parser.parse_args()
 
     IDF_PATH = os.environ['IDF_PATH']
@@ -41,20 +51,36 @@ if __name__ == '__main__':
         SUPPORTED_TARGETS.append('linux')
         ignore_warning = 'warning: '  # Ignore all common warnings on linux builds
     setup_logging(2)
-    apps = find_apps(
-        args.paths,
-        recursive=args.recursive,
-        target=args.target,
-        build_dir='build_@t_@w',
-        config_rules_str=args.rules,
-        build_log_path='build_log.txt',
-        size_json_path='size.json' if not args.linux else None,
-        check_warnings=True,
-        preserve=not args.delete,
-        manifest_files=args.manifests,
-        default_build_targets=SUPPORTED_TARGETS,
-        manifest_rootpath='.',
-    )
+    if version.parse(get_distribution('idf_build_apps').version) >= version.parse('2.0.0'):
+        apps = find_apps(
+            args.paths,
+            recursive=args.recursive,
+            target=args.target,
+            build_dir='build_@t_@w',
+            config_rules_str=args.rules,
+            build_log_filename='build_log.txt',
+            size_json_filename='size.json' if not args.linux else None,
+            check_warnings=True,
+            preserve=not args.delete,
+            manifest_files=args.manifests,
+            default_build_targets=SUPPORTED_TARGETS,
+            manifest_rootpath='.',
+        )
+    else:
+        apps = find_apps(
+            args.paths,
+            recursive=args.recursive,
+            target=args.target,
+            build_dir='build_@t_@w',
+            config_rules_str=args.rules,
+            build_log_path='build_log.txt',
+            size_json_path='size.json' if not args.linux else None,
+            check_warnings=True,
+            preserve=not args.delete,
+            manifest_files=args.manifests,
+            default_build_targets=SUPPORTED_TARGETS,
+            manifest_rootpath='.',
+        )
 
     for app in apps:
         print(app)
