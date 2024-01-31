b7b59f613d0f452b076ec2c75fb6e02467319ac2
Abhik Roy <abhik.roy@espressif.com>
2024-01-31 23:22:45 +1100

    feat(console): Added component with mqtt command

---
+++ components/console_cmd_mqtt/examples/mqtt-basic/pytest_mqtt_basic.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Unlicense OR CC0-1.0
+
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.mark.esp32
+def test_examples_mqtt_command(dut):
+    dut.expect('esp>', timeout=30)
+    dut.write('help mqtt')
+    dut.expect('mqtt  [-CPSULD] [-t <topic>] [-d <data>]', timeout=30)
+    pass
