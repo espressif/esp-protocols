dfb0035858e1f23e4bec0f30df42d00844e937f3
David Cermak <cermak@espressif.com>
2024-03-04 20:26:02 +0100

    feat(wifi_remote): Added generation step for wifi_remote based on IDF

--- components/esp_wifi_remote/esp_wifi_remote_net.c
+++ components/esp_wifi_remote/esp_wifi_remote_net.c
@@ -9,26 +9,25 @@
 #include "esp_log.h"
 
 #define CHANNELS 2
+#define WEAK __attribute__((weak))
 
 static esp_remote_channel_tx_fn_t s_tx_cb[CHANNELS];
 static esp_remote_channel_t s_channel[CHANNELS];
 static wifi_rxcb_t s_rx_fn[CHANNELS];
 
-esp_err_t esp_wifi_remote_channel_rx(void *h, void *buffer, void *buff_to_free, size_t len)
+WEAK esp_err_t esp_wifi_remote_channel_rx(void *h, void *buffer, void *buff_to_free, size_t len)
 {
     assert(h);
-    if (h == s_channel[0]) {
-        assert(s_rx_fn[0]);
+    if (h == s_channel[0] && s_rx_fn[0]) {
         return s_rx_fn[0](buffer, len, buff_to_free);
     }
-    if (h == s_channel[1]) {
-        assert(s_rx_fn[1]);
+    if (h == s_channel[1] && s_rx_fn[1]) {
         return s_rx_fn[1](buffer, len, buff_to_free);
     }
     return ESP_FAIL;
 }
 
-esp_err_t esp_wifi_remote_channel_set(wifi_interface_t ifx, void *h, esp_remote_channel_tx_fn_t tx_cb)
+WEAK esp_err_t esp_wifi_remote_channel_set(wifi_interface_t ifx, void *h, esp_remote_channel_tx_fn_t tx_cb)
 {
     if (ifx == WIFI_IF_STA) {
         s_channel[0] = h;
@@ -43,38 +42,38 @@ esp_err_t esp_wifi_remote_channel_set(wifi_interface_t ifx, void *h, esp_remote_
     return ESP_FAIL;
 }
 
+WEAK esp_err_t esp_wifi_internal_set_sta_ip(void)
+{
+    // TODO: Pass this information to the slave target
+    // Note that this function is called from the default event loop, so we shouldn't block here
+    return ESP_OK;
+}
 
-esp_err_t esp_wifi_internal_set_sta_ip(void)
+WEAK esp_err_t esp_wifi_internal_reg_netstack_buf_cb(wifi_netstack_buf_ref_cb_t ref, wifi_netstack_buf_free_cb_t free)
 {
     return ESP_OK;
 }
 
-esp_err_t esp_wifi_internal_reg_netstack_buf_cb(wifi_netstack_buf_ref_cb_t ref, wifi_netstack_buf_free_cb_t free)
+WEAK void esp_wifi_internal_free_rx_buffer(void *buffer)
 {
-    return ESP_OK;
+    free(buffer);
 }
 
-void esp_wifi_internal_free_rx_buffer(void *buffer)
+WEAK int esp_wifi_internal_tx(wifi_interface_t ifx, void *buffer, uint16_t len)
 {
-    if (buffer) {
-        free(buffer);
-    }
-}
+    if (ifx == WIFI_IF_STA && s_tx_cb[0]) {
 
-int esp_wifi_internal_tx(wifi_interface_t ifx, void *buffer, uint16_t len)
-{
-    if (ifx == WIFI_IF_STA) {
         /* TODO: If not needed, remove arg3 */
         return s_tx_cb[0](s_channel[0], buffer, len);
     }
-    if (ifx == WIFI_IF_AP) {
+    if (ifx == WIFI_IF_AP && s_tx_cb[1]) {
         return s_tx_cb[1](s_channel[1], buffer, len);
     }
 
     return -1;
 }
 
-esp_err_t esp_wifi_internal_reg_rxcb(wifi_interface_t ifx, wifi_rxcb_t fn)
+WEAK esp_err_t esp_wifi_internal_reg_rxcb(wifi_interface_t ifx, wifi_rxcb_t fn)
 {
     if (ifx == WIFI_IF_STA) {
         ESP_LOGI("esp_wifi_remote", "%s: sta: %p", __func__, fn);
