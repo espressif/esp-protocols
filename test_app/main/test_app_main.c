eb84eed8d3346940bc0938d75708b7f38b501b9e
David Cermak <cermak@espressif.com>
2024-04-05 12:41:41 +0200

    ci(common): Add clang tidy check to esp-protocols

---
+++ test_app/main/test_app_main.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2015-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "unity.h"
+
+void app_main(void)
+{
+    UNITY_BEGIN();
+    unity_run_all_tests();
+    UNITY_END();
+}
