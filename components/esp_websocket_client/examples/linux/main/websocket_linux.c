5ccc018a982c045776a99701ed30e77ef26ebfcc
Suren Gabrielyan <suren.gabrielyan@espressif.com>
2024-06-26 13:28:23 +0400

    fix(websocket): fix of websocket host example

--- components/esp_websocket_client/examples/linux/main/websocket_linux.c
+++ components/esp_websocket_client/examples/linux/main/websocket_linux.c
@@ -3,20 +3,13 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#include <stdio.h>
-#include "esp_log.h"
+#include <esp_log.h>
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/event_groups.h"
 
 #include "esp_websocket_client.h"
 #include "esp_system.h"
 #include "esp_event.h"
-#include "esp_log.h"
 #include "esp_netif.h"
 
 static const char *TAG = "websocket";
