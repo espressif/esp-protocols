e14e21ab324d47ea27aa209069cb9b95fc278c7a
Euripedes Rocha <rocha.euripedes@espressif.com>
2025-03-05 15:24:09 +0100

    bump(mosq): 2.0.20~1 -> 2.0.20~2
    ##
    [2.0.20~2](https://github.com/espressif/esp-protocols/commits/mosq-v2.0.20_2)###
    Features- Allow user to enable SYS topic
    ([905b84fb](https://github.com/espressif/esp-protocols/commit/905b84fb))###
    Bug Fixes- Remove temp modification of IDF files
    ([9162de11](https://github.com/espressif/esp-protocols/commit/9162de11))-
    Add a note about stack size
    ([dbd164dd](https://github.com/espressif/esp-protocols/commit/dbd164dd))

--- components/mosquitto/port/mosq_time.c
+++ components/mosquitto/port/mosq_time.c
@@ -1,7 +1,9 @@
 /*
- * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2024 Roger Light <roger@atchoo.org>
  *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * SPDX-FileContributor: 2025 Espressif Systems (Shanghai) CO LTD
  */
 
 #include <time.h>
