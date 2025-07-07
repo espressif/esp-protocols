14d3cb6bd17418dd11d9c0143ddd8f3531e5804e
David Cermak <cermak@espressif.com>
2025-07-07 17:12:29 +0200

    fix(modem): Add support for ESP-AT based tcp-client example

---
+++ components/esp_modem/examples/modem_tcp_client/main/generic_module/sock_module.hpp
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "esp_modem_config.h"
+#include "cxx_include/esp_modem_api.hpp"
+
+#pragma  once
+
+namespace sock_dce {
+
+using Module = esp_modem::GenericModule;
+
+}
