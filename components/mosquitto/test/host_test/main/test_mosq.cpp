3f4550815934f1a8dc05040cb857349b617cb572
David Cermak <cermak@espressif.com>
2026-03-05 10:48:03 +0100

    fix(mosq): Fix mosquitto restart issue
    Aslo adds a unit test
    
    Closes https://github.com/espressif/esp-protocols/issues/1022

--- components/mosquitto/test/host_test/main/test_mosq.cpp
+++ components/mosquitto/test/host_test/main/test_mosq.cpp
@@ -31,6 +31,31 @@ TEST_CASE("Start and stop mosquitto broker", "[mosquitto]")
     CHECK(broker_rc == 0);
 }
 
+TEST_CASE("Restart mosquitto broker after stop", "[mosquitto]")
+{
+    struct mosq_broker_config config = {};
+    config.host = "0.0.0.0";
+    config.port = 18834;
+
+    auto run_broker = [&]() {
+        int broker_rc = -1;
+        std::thread broker_thread([&]() {
+            broker_rc = mosq_broker_run(&config);
+        });
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+        mosq_broker_stop();
+
+        broker_thread.join();
+
+        return broker_rc;
+    };
+
+    CHECK(run_broker() == 0);
+    CHECK(run_broker() == 0);
+}
+
 extern "C" void app_main(void)
 {
     int result = Catch::Session().run();
