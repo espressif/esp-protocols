64a587505e0518f5c673156726aef4c1f54b6760
David Cermak <cermak@espressif.com>
2026-04-17 14:12:01 +0200

    fix(asio): Fix asio dependency issue
    sock_utils is a private dependency, but is included in a public header
    Fix: guard the inclusion to only internal ASIO_SOURCE
    
    Closes https://github.com/espressif/esp-protocols/issues/1044

---
+++ components/asio/tests/build_test/components/asio_consumer/include/asio_consumer.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void asio_consumer_run(void);
+
+#ifdef __cplusplus
+}
+#endif
