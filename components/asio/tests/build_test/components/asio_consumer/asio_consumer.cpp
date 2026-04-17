13777e3b47835e6b6ade7b2c3185807b7d17f27d
David Cermak <cermak@espressif.com>
2026-04-17 18:26:37 +0200

    fix(asio): Revert the sock-utils public deps and guard its header

--- components/asio/tests/build_test/components/asio_consumer/asio_consumer.cpp
+++ components/asio/tests/build_test/components/asio_consumer/asio_consumer.cpp
@@ -4,11 +4,22 @@
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
 #include "asio.hpp"
+#include "asio/local/connect_pair.hpp"
+#include "asio/local/stream_protocol.hpp"
 #include "asio_consumer.h"
 
 void asio_consumer_run(void)
 {
     asio::io_context io_context;
-    asio::ip::tcp::socket socket(io_context);
-    (void)socket;
+    asio::ip::tcp::socket tcp_sock(io_context);
+    (void)tcp_sock;
+
+    // Exercise asio::local::connect_pair: this is the only public client-facing
+    // API that ultimately calls ::socketpair() (through asio's separately
+    // compiled TU). If sock_utils were leaking through asio's public headers,
+    // this would require sock_utils on the component's include path.
+    asio::local::stream_protocol::socket s1(io_context), s2(io_context);
+    asio::error_code ec;
+    asio::local::connect_pair(s1, s2, ec);
+    (void)ec;
 }
