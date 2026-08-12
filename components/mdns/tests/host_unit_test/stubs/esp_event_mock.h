8e2f3c7cee86bf36a06c8ad2768b3efd76732658
zwx <zhangwenxu@espressif.com>
2026-08-12 19:38:42 +0800

    test(mdns): add host event mock

---
+++ components/mdns/tests/host_unit_test/stubs/esp_event_mock.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+#pragma once
+
+#include "esp_event.h"
+
+void mdns_test_esp_event_reset(void);
